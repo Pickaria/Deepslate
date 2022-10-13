f5d18c382f5fd386f34a1a7dcc23c470c4eaf37f
Erwan <erwan.legloannec@outlook.com>
2022-10-13 18:28:23 +0200

    wip: jobs rewards

--- plugins/job/src/main/kotlin/fr/pickaria/job/JobMenu.kt
+++ plugins/job/src/main/kotlin/fr/pickaria/job/JobMenu.kt
@@ -6,11 +6,11 @@ import org.bukkit.Material
 import org.bukkit.entity.HumanEntity
 import org.bukkit.entity.Player
 
-class JobMenu(title: String, opener: HumanEntity?, previousMenu: BaseMenu?, size: Int = 54) :
+class JobMenu(title: String, opener: HumanEntity, previousMenu: BaseMenu?, size: Int = 54) :
 	BaseMenu(title, opener, previousMenu, size) {
 
 	class Factory: BaseMenu.Factory("§6§lMétiers", Material.WOODEN_PICKAXE) {
-		override fun create(opener: HumanEntity?, previousMenu: BaseMenu?, size: Int): BaseMenu = JobMenu(title, opener, previousMenu, size)
+		override fun create(opener: HumanEntity, previousMenu: BaseMenu?, size: Int): BaseMenu = JobMenu(title, opener, previousMenu, size)
 	}
 
 	init {
@@ -18,31 +18,56 @@ class JobMenu(title: String, opener: HumanEntity?, previousMenu: BaseMenu?, size
 	}
 
 	override fun initMenu() {
-		var x = 1
+		val playerJobs = Job.get(opener.uniqueId).associateBy { it.job }
+
+		var x = 4 - playerJobs.size / 2
+		val y = if (playerJobs.isEmpty()) {
+			2
+		} else {
+			3
+		}
+
+		playerJobs.forEach { (key, job) ->
+			if (job.active) {
+				val config = jobConfig.jobs[key]
+
+				config?.let {
+					setMenuItem {
+						this.x = x++
+						this.y = 1
+						material = it.icon
+						name = "Métier actuel"
+						lore = listOf("§7${config.label}", "§6Récompense en attente de récupération")
+						isEnchanted = true
+					}
+				}
+			}
+		}
+
+		x = 1
 
 		jobConfig.jobs.forEach { (key, job) ->
+
 			val lore = mutableListOf<String>()
 			job.description.forEach {
 				lore.add("§7${it}")
 			}
 
-			opener?.let { opener ->
-				Job.get(opener.uniqueId, key)?.let {
-					val level = jobController.getLevelFromExperience(job, it.experience)
-					lore.add("§6Expérience totale :§7 ${it.experience}")
-					lore.add("§6Niveau :§7 $level")
-				}
+			playerJobs[key]?.let {
+				val level = jobController.getLevelFromExperience(job, it.experience)
+				lore.add("§6Expérience totale :§7 ${it.experience}")
+				lore.add("§6Niveau :§7 $level")
+			} ?: lore.add("§7Métier pas encore exercé")
 
-				if (!jobController.hasJob(opener.uniqueId, key)) {
-					lore.add("Clic-gauche pour rejoindre le métier")
-				} else {
-					lore.add("Clic-droit pour quitter le métier")
-				}
+			if (!jobController.hasJob(opener.uniqueId, key)) {
+				lore.add("Clic-gauche pour rejoindre le métier")
+			} else {
+				lore.add("Clic-droit pour quitter le métier")
 			}
 
 			setMenuItem {
 				this.x = x++
-				y = 2
+				this.y = y
 				material = job.icon
 				name = job.label
 				this.lore = lore
--- plugins/menu/src/main/kotlin/fr/pickaria/menu/sub/HomeMenu.kt
+++ plugins/menu/src/main/kotlin/fr/pickaria/menu/sub/HomeMenu.kt
@@ -5,11 +5,11 @@ import fr.pickaria.menu.menuController
 import org.bukkit.Material
 import org.bukkit.entity.HumanEntity
 
-class HomeMenu(title: String, opener: HumanEntity?, previousMenu: BaseMenu?, size: Int = 54) :
+class HomeMenu(title: String, opener: HumanEntity, previousMenu: BaseMenu?, size: Int = 54) :
 	BaseMenu(title, opener, previousMenu, size) {
 
 	class Factory: BaseMenu.Factory("§6§lAccueil", Material.GRASS_BLOCK) {
-		override fun create(opener: HumanEntity?, previousMenu: BaseMenu?, size: Int): BaseMenu = HomeMenu(title, opener, previousMenu, size)
+		override fun create(opener: HumanEntity, previousMenu: BaseMenu?, size: Int): BaseMenu = HomeMenu(title, opener, previousMenu, size)
 	}
 
 	override fun initMenu() {
