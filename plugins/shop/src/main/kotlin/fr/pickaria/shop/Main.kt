4844c46494ed2e9a9e5a2d185486a926dc310934
Erwan <erwan.legloannec@outlook.com>
2022-10-15 17:11:16 +0200

    merchants

--- plugins/shop/src/main/kotlin/fr/pickaria/shop/Main.kt
+++ plugins/shop/src/main/kotlin/fr/pickaria/shop/Main.kt
@@ -1,5 +1,6 @@
 package fr.pickaria.shop
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
@@ -7,7 +8,8 @@ class Main : JavaPlugin() {
 		super.onEnable()
 
 		getCommand("test")?.setExecutor(TestCommand()) ?: server.logger.warning("Command `test` could not be registered")
-		getCommand("x")?.setExecutor(TestMenu()) ?: server.logger.warning("Command `x` could not be registered")
+
+		Bukkit.getServer().pluginManager.registerEvents(TestMenu(), this)
 
 		logger.info("Shop plugin loaded!")
 	}
--- plugins/shop/src/main/kotlin/fr/pickaria/shop/TestMenu.kt
+++ plugins/shop/src/main/kotlin/fr/pickaria/shop/TestMenu.kt
@@ -1,20 +1,64 @@
 package fr.pickaria.shop
 
+import net.kyori.adventure.text.Component
 import org.bukkit.Bukkit
+import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryOpenEvent
 import org.bukkit.event.inventory.InventoryType
+import org.bukkit.event.inventory.TradeSelectEvent
+import org.bukkit.inventory.ItemStack
+import org.bukkit.inventory.MerchantRecipe
+import kotlin.math.floor
 
-class TestMenu: CommandExecutor {
-	override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
-		if (sender is Player) {
-			val inventory = Bukkit.createInventory(sender, InventoryType.MERCHANT)
-			sender.openInventory(inventory)
-			inventory.clear()
+class TestMenu : Listener {
+	@EventHandler
+	fun onTradeSelected(event: TradeSelectEvent) {
+		event.whoClicked.sendMessage("${event.index}")
+	}
+
+	@EventHandler
+	fun onChestOpened(event: InventoryOpenEvent) {
+		if (event.player.isOnGround && event.inventory.type == InventoryType.CHEST) {
+			val merchant = Bukkit.createMerchant(Component.text("Shop"))
+
+			val recipes = event.inventory.contents
+				.filterNotNull()
+				.map {
+					it.clone().apply {
+						amount = 1
+					}
+				}
+				.filter {
+					!it.type.isAir
+				}
+				.toSet()
+
+			merchant.recipes = recipes.mapNotNull {
+				if (!it.type.isAir) {
+					val merchantRecipe = MerchantRecipe(it, Integer.MAX_VALUE)
+					merchantRecipe.uses = Integer.MIN_VALUE
+					merchantRecipe.addIngredient(
+						ItemStack(
+							Material.ECHO_SHARD,
+							(floor(Math.random() * 64) + 1).toInt()
+						)
+					)
+
+					merchantRecipe
+				} else {
+					null
+				}
+			}
+
+			event.player.openMerchant(merchant, true)
+
+			event.isCancelled = true
 		}
-
-		return true
 	}
 }
