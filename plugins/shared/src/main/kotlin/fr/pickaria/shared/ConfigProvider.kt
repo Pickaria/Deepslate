9c20f905484edf72e64437d97904f8aa953084ec
Erwan <erwan.legloannec@outlook.com>
2022-10-24 19:08:24 +0200

    deserialize mini message

--- plugins/shared/src/main/kotlin/fr/pickaria/shared/ConfigProvider.kt
+++ plugins/shared/src/main/kotlin/fr/pickaria/shared/ConfigProvider.kt
@@ -1,29 +1,43 @@
 package fr.pickaria.shared
 
 import net.kyori.adventure.text.Component
-import org.bukkit.configuration.file.FileConfiguration
+import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.configuration.ConfigurationSection
 import kotlin.reflect.KProperty
 
-val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
+internal val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
 
 fun String.toSnakeCase(): String = camelRegex.replace(this) {
 	"_${it.value}"
 }.lowercase()
 
-class FileConfigurationDelegator {
-	var config: FileConfiguration? = null
+abstract class ConfigProvider {
+	var section: ConfigurationSection? = null
+	val deserializer: MiniMessageDeserializer = MiniMessageDeserializer()
 
 	inline operator fun <reified T> getValue(thisRef: ConfigProvider, property: KProperty<*>): T =
 		with(property.name.toSnakeCase()) {
 			when (T::class) {
-				Component::class -> config?.getString(this)?.let { Component.text(it) } as? T
-				else -> config?.get(this) as? T
+				Component::class -> {
+					section?.getString(this)?.let { Component.text(it) } as? T
+				}
+
+				else -> {
+					section?.get(this) as? T
+				}
 			} ?: throw Exception("Property $this not found in config file.")
 		}
-}
 
-abstract class ConfigProvider(val config: FileConfigurationDelegator = FileConfigurationDelegator()) {
-	fun setConfig(config: FileConfiguration) {
-		this.config.config = config
+	fun setConfig(config: ConfigurationSection) {
+		this.section = config
+	}
+
+	inner class MiniMessageDeserializer {
+		private val miniMessage: MiniMessage = MiniMessage.miniMessage()
+
+		operator fun getValue(config: ConfigProvider, property: KProperty<*>): Component =
+			section?.getString(property.name.toSnakeCase())?.let {
+				miniMessage.deserialize(it)
+			} ?: Component.text("")
 	}
 }
