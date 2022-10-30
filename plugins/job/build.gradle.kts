e8072007264e23e928efc62dec471979b5520b69
Erwan <erwan.legloannec@outlook.com>
2022-10-30 20:09:40 +0100

    update deps

--- plugins/job/build.gradle.kts
+++ plugins/job/build.gradle.kts
@@ -3,11 +3,11 @@ repositories {
 }
 
 dependencies {
-	compileOnly(project(":lib"))
 	compileOnly(project(":database"))
-	compileOnly(project(":shared"))
 	compileOnly(project(":economy"))
+	compileOnly(project(":lib"))
 	compileOnly(project(":menu"))
 	compileOnly(project(":potion"))
+	compileOnly(project(":shared"))
 	compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
 }
