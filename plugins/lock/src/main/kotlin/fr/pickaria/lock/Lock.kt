78a029785dc90f35966d38fa7cdba51a60bcedae
SevRusse <anthony.anthony.moureau@gmail.com>
2022-11-18 21:25:17 +0100

    Updated Lock.kt
    Loker is working, not the signs.
    Protection Signs are placed on the good block, but the facing is wrong.
    No text is displayed: miss some updates.

--- plugins/lock/src/main/kotlin/fr/pickaria/lock/Lock.kt
+++ plugins/lock/src/main/kotlin/fr/pickaria/lock/Lock.kt
@@ -1,10 +1,15 @@
 package fr.pickaria.lock
 
+import net.kyori.adventure.text.Component
 import org.bukkit.GameMode
 import org.bukkit.Material
-import org.bukkit.Tag
+import org.bukkit.block.Block
+import org.bukkit.block.BlockFace
 import org.bukkit.block.Lockable
 import org.bukkit.block.Sign
+import org.bukkit.block.data.Directional
+import org.bukkit.block.data.Rotatable
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockDamageEvent
@@ -12,8 +17,8 @@ import org.bukkit.event.block.BlockDamageEvent
 /**
  *     #--------- WORK IN PROGRESS
  *
- * 'Lock' is made for protecting containers.
- *   - Locked containers have a sign against one of the face.
+ * 'Lock' is made for lockable containers.
+ *   - Locked containers have a sign against one of their faces (must be `Material.AIR`).
  * A locked container have an owner.
  *   - The owner can have trusted players on this container.
  *   - Only the owner can break the protection.
@@ -27,22 +32,6 @@ class Lock : Listener {
     companion object {
         // header of the sign, indicates that the container is locked
         const val lockWord = "§6§1[Protected]"
-        val lockBlocks = setOf(
-            Material.BARREL,
-            Material.BEACON,
-            Material.BLAST_FURNACE,
-            Material.BREWING_STAND,
-            Material.CHEST,
-            Material.DISPENSER,
-            Material.DROPPER,
-            Material.ENDER_CHEST,
-            Material.FURNACE,
-            Material.HOPPER,
-            Material.SHULKER_BOX,
-            Material.SMOKER,
-            Material.TRAPPED_CHEST,
-        )
-        val lockSigns = setOf(Tag.SIGNS)
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -51,20 +40,49 @@ class Lock : Listener {
          * When a player tries to interact with a container, this function is called.
          */
         val player = event.player
-        if (player.gameMode == GameMode.CREATIVE) return
-        if (!player.isSneaking) return
+        if (player.gameMode == GameMode.CREATIVE || !player.isSneaking) return
 
         val block = event.block
         when (block.state) {
-            is Lockable -> { // TODO: isLocked, getKey, setKey
-                player.sendMessage("hit Lockable....")
+            is Lockable -> {
+                val container = block.state as Lockable
+                if (container.isLocked) unlock(block) else lock(block, player)
             }
             is Sign -> {
-                player.sendMessage("hit Sign....")
+                val sign = block.state as Sign
+                if (isProtectionSign(sign)) unlockFromProtectionSign(sign) else return
             }
             else -> return
         }
-        player.sendMessage("Lock is triggered.")
     }
 
+    private fun isProtectionSign(sign: Sign): Boolean {
+        return ((sign.line(0).toString() == lockWord)
+                && (sign.line(1) != Component.text("")))
+    }
+
+    private fun lock(container: Block, player: Player) {
+        player.sendMessage("Going to lock this Lockable....")
+        val lookAtFace = player.getTargetBlockFace(5)!!
+        player.sendMessage("looking on a face: $lookAtFace....")
+        placeProtectionSign(container.getRelative(lookAtFace), lookAtFace, player)
+        // last task!
+        (container.state as Lockable).setLock(player.uniqueId.toString())
+    }
+
+    private fun unlock(container: Block) {
+        //TODO: look for Protection Sign, especially for Double Chests.
+        (container.state as Lockable).setLock("")
+    }
+
+    private fun unlockFromProtectionSign(sign: Sign) {}
+
+    private fun placeProtectionSign(block: Block, face: BlockFace, player: Player) {
+        player.sendMessage("trying to place a sign on: ${block.state.location}....")
+        block.type = Material.ACACIA_WALL_SIGN
+        (block.blockData as Directional).facing = face
+        val sign = block.state as Sign
+        sign.line(0, Component.text(lockWord))
+        sign.line(1, Component.text(player.name))
+    }
 }
