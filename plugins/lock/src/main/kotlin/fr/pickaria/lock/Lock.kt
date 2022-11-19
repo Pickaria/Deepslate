a05cd6565ce32d597e37c7ce04db3b3266d18198
Anthony <75372378+SevRusse@users.noreply.github.com>
2022-11-19 21:33:35 +0100

    Update Lock
    Protection Signs now display text, but facings are still wrong.
    Have to learn how to manage these !

--- plugins/lock/src/main/kotlin/fr/pickaria/lock/Lock.kt
+++ plugins/lock/src/main/kotlin/fr/pickaria/lock/Lock.kt
@@ -3,24 +3,27 @@ package fr.pickaria.lock
 import net.kyori.adventure.text.Component
 import org.bukkit.GameMode
 import org.bukkit.Material
+import org.bukkit.NamespacedKey
 import org.bukkit.block.Block
 import org.bukkit.block.BlockFace
 import org.bukkit.block.Lockable
 import org.bukkit.block.Sign
 import org.bukkit.block.data.Directional
-import org.bukkit.block.data.Rotatable
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
 import org.bukkit.event.block.BlockDamageEvent
+import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
 
 /**
  *     #--------- WORK IN PROGRESS
  *
- * 'Lock' is made for lockable containers.
+ * 'Lock' is made for Lockable containers.
  *   - Locked containers have a sign against one of their faces (must be `Material.AIR`).
- * A locked container have an owner.
- *   - The owner can have trusted players on this container.
+ *   - A locked container have an owner.
+ *   - The owner can have trusted players on its container.
  *   - Only the owner can break the protection.
  *   - Trusted members can access the container, but not break the protection.
  */
@@ -56,9 +59,14 @@ class Lock : Listener {
         }
     }
 
+    @EventHandler(ignoreCancelled = true)
+    fun onBlockBreak(event: BlockBreakEvent) {
+        //TODO: control thieves and bypasses (one-click break)
+    }
+
     private fun isProtectionSign(sign: Sign): Boolean {
-        return ((sign.line(0).toString() == lockWord)
-                && (sign.line(1) != Component.text("")))
+    return ((sign.line(0) == Component.text(lockWord))
+            && (sign.line(1) != Component.text("")))
     }
 
     private fun lock(container: Block, player: Player) {
@@ -66,7 +74,8 @@ class Lock : Listener {
         val lookAtFace = player.getTargetBlockFace(5)!!
         player.sendMessage("looking on a face: $lookAtFace....")
         placeProtectionSign(container.getRelative(lookAtFace), lookAtFace, player)
-        // last task!
+        // last tasks!
+        (container.blockData as PersistentDataContainer).set(NamespacedKey.fromString(container.location.toString())!!, PersistentDataType.STRING, player.uniqueId.toString())
         (container.state as Lockable).setLock(player.uniqueId.toString())
     }
 
@@ -80,9 +89,12 @@ class Lock : Listener {
     private fun placeProtectionSign(block: Block, face: BlockFace, player: Player) {
         player.sendMessage("trying to place a sign on: ${block.state.location}....")
         block.type = Material.ACACIA_WALL_SIGN
-        (block.blockData as Directional).facing = face
         val sign = block.state as Sign
+        val signDir = (block.blockData as Directional)
+        signDir.facing = face
+        block.blockData = signDir
         sign.line(0, Component.text(lockWord))
         sign.line(1, Component.text(player.name))
+        sign.update()
     }
 }
