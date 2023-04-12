88f6cb0c0961a34d2c0274d5220a076c6d7e258a
Erwan <erwan.legloannec@outlook.com>
2023-04-12 10:32:52 +0200

    Migrate to paper plugin and remove lib catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,39 +2,42 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
 	id("java")
-	kotlin("jvm")
-	kotlin("plugin.serialization")
-	id("com.github.johnrengelman.shadow")
-	id("fr.pickaria.redstone")
+	kotlin("jvm") version "1.8.20"
+	kotlin("plugin.serialization") version "1.8.20"
 }
 
 group = "fr.pickaria"
 version = "1.0-SNAPSHOT"
 
+repositories {
+	mavenCentral()
+	maven("https://oss.sonatype.org/content/groups/public/")
+	maven("https://repo.papermc.io/repository/maven-public/") // Paper
+	maven("https://maven.quozul.dev/snapshots")
+	maven("https://repo.aikar.co/content/groups/aikar/") // ACF
+	maven("https://jitpack.io") // Vault
+	maven("https://repo.glaremasters.me/repository/towny/") // Towny
+}
+
 dependencies {
-	testImplementation(kotlin("test"))
-	implementation(libs.kotlin.stdlib)
-	implementation(libs.kotlin.reflect)
-	implementation(libs.kotlinx.datetime)
-	implementation(libs.kotlinx.serialization.json)
-
-	compileOnly(libs.paper)
-	compileOnly(libs.vault)
-	implementation(libs.acf)
-	implementation(libs.mccoroutine.api)
-	implementation(libs.mccoroutine.core)
-	compileOnly(libs.towny)
-
-	implementation(libs.exposed.core)
-	implementation(libs.exposed.dao)
-	implementation(libs.exposed.jdbc)
-	implementation(libs.exposed.kotlin.datetime)
-	implementation(libs.h2)
-
-	compileOnly(libs.pickaria.spawner)
-	implementation(libs.pickaria.bedrock)
-
-	implementation(libs.kaml)
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
+	compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
+	compileOnly("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+	compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+	compileOnly("co.aikar:acf-paper:0.5.1-SNAPSHOT")
+	compileOnly("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.11.0")
+	compileOnly("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.11.0")
+	compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+	compileOnly("com.palmergames.bukkit.towny:towny:0.99.0.0")
+	compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
+	compileOnly("org.jetbrains.exposed:exposed-core:0.41.1")
+	compileOnly("org.jetbrains.exposed:exposed-dao:0.41.1")
+	compileOnly("org.jetbrains.exposed:exposed-jdbc:0.41.1")
+	compileOnly("org.jetbrains.exposed:exposed-kotlin-datetime:0.41.1")
+	compileOnly("com.h2database:h2:2.1.214")
+	compileOnly("fr.pickaria:bedrock:1.0.18-SNAPSHOT")
+	compileOnly("fr.pickaria:spawner:1.0.8-SNAPSHOT")
+	compileOnly("com.charleskorn.kaml:kaml:0.53.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -42,12 +45,13 @@ tasks.withType<KotlinCompile> {
 }
 
 tasks {
-	shadowJar {
-		destinationDirectory.set(file("$rootDir/server/plugins")) // Output to test server's plugins folder
-	}
-
 	compileKotlin {
 		kotlinOptions.javaParameters = true
+	}
+
+	jar {
+		destinationDirectory.set(file("server/plugins"))
+		from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
 	}
 }
 
