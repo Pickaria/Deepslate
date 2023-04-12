0dc8663c53c937ce90624537df7548307463500b
Erwan <erwan.legloannec@outlook.com>
2023-04-12 11:51:17 +0200

    Include all dependencies in final jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,24 +20,25 @@ repositories {
 }
 
 dependencies {
-	compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
-	compileOnly("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
-	compileOnly("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-	compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
-	compileOnly("co.aikar:acf-paper:0.5.1-SNAPSHOT")
-	compileOnly("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.11.0")
-	compileOnly("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.11.0")
 	compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
 	compileOnly("com.palmergames.bukkit.towny:towny:0.99.0.0")
 	compileOnly("com.github.MilkBowl:VaultAPI:1.7.1")
-	compileOnly("org.jetbrains.exposed:exposed-core:0.41.1")
-	compileOnly("org.jetbrains.exposed:exposed-dao:0.41.1")
-	compileOnly("org.jetbrains.exposed:exposed-jdbc:0.41.1")
-	compileOnly("org.jetbrains.exposed:exposed-kotlin-datetime:0.41.1")
-	compileOnly("com.h2database:h2:2.1.214")
-	compileOnly("fr.pickaria:bedrock:1.0.19-SNAPSHOT")
 	compileOnly("fr.pickaria:spawner:1.0.8-SNAPSHOT")
-	compileOnly("com.charleskorn.kaml:kaml:0.53.0")
+
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.20")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.20")
+	implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+	implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+	implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
+	implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.11.0")
+	implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.11.0")
+	implementation("org.jetbrains.exposed:exposed-core:0.41.1")
+	implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
+	implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
+	implementation("org.jetbrains.exposed:exposed-kotlin-datetime:0.41.1")
+	implementation("com.h2database:h2:2.1.214")
+	implementation("fr.pickaria:bedrock:1.0.19-SNAPSHOT")
+	implementation("com.charleskorn.kaml:kaml:0.53.0")
 }
 
 tasks.withType<KotlinCompile> {
@@ -50,8 +51,15 @@ tasks {
 	}
 
 	jar {
+		duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
 		destinationDirectory.set(file("server/plugins"))
-		from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
+
+		manifest {
+			attributes["Main-Class"] = "fr.pickaria.MainKt"
+		}
+
+		from(configurations.runtimeClasspath.get().map { zipTree(it) })
 	}
 }
 
