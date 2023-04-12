554d380d86cf44a6311e78a9bc1594df26555a97
Erwan <erwan.legloannec@outlook.com>
2023-04-12 09:57:03 +0200

    Remove library sub-project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+	id("java")
 	kotlin("jvm")
+	kotlin("plugin.serialization")
 	id("com.github.johnrengelman.shadow")
+	id("fr.pickaria.redstone")
 }
 
 group = "fr.pickaria"
@@ -10,15 +13,17 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
 	testImplementation(kotlin("test"))
-	implementation(libs.kotlin.reflect)
 	implementation(libs.kotlin.stdlib)
+	implementation(libs.kotlin.reflect)
 	implementation(libs.kotlinx.datetime)
 	implementation(libs.kotlinx.serialization.json)
 
 	compileOnly(libs.paper)
+	compileOnly(libs.vault)
 	implementation(libs.acf)
 	implementation(libs.mccoroutine.api)
 	implementation(libs.mccoroutine.core)
+	compileOnly(libs.towny)
 
 	implementation(libs.exposed.core)
 	implementation(libs.exposed.dao)
@@ -26,6 +31,7 @@ dependencies {
 	implementation(libs.exposed.kotlin.datetime)
 	implementation(libs.h2)
 
+	compileOnly(libs.pickaria.spawner)
 	implementation(libs.pickaria.bedrock)
 
 	implementation(libs.kaml)
@@ -35,6 +41,22 @@ tasks.withType<KotlinCompile> {
 	kotlinOptions.jvmTarget = "17"
 }
 
-tasks.shadowJar {
-	destinationDirectory.set(file("$rootDir/server/plugins")) // Output to test server's plugins folder
+tasks {
+	shadowJar {
+		destinationDirectory.set(file("$rootDir/server/plugins")) // Output to test server's plugins folder
+	}
+
+	compileKotlin {
+		kotlinOptions.javaParameters = true
+	}
+}
+
+val targetJavaVersion = 17
+java {
+	val javaVersion = JavaVersion.toVersion(targetJavaVersion)
+	sourceCompatibility = javaVersion
+	targetCompatibility = javaVersion
+	if (JavaVersion.current() < javaVersion) {
+		toolchain.languageVersion.set(JavaLanguageVersion.of(targetJavaVersion))
+	}
 }
--- libraries/src/main/kotlin/fr/pickaria/Main.kt
+++
@@ -1,5 +0,0 @@
-package fr.pickaria
-
-import org.bukkit.plugin.java.JavaPlugin
-
-class Main: JavaPlugin()
