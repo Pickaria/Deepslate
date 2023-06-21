0f269076c07024a7aa4e903886ffc6bb6bafbafc
Hajïn_ <44238262+AHajin@users.noreply.github.com>
2023-06-21 06:05:53 +0200

    Revert "Revert "Tpa|TpDeny|TpCancel|TpYes""
    This reverts commit 0c809932380f86bd63a5e2fd6e5eabf3ffadb578.

--- src/main/kotlin/fr/pickaria/vue/teleport/HomeTeleport.kt
+++ src/main/kotlin/fr/pickaria/vue/teleport/HomeTeleport.kt
@@ -79,7 +79,7 @@ class HomeTeleport(private val plugin: JavaPlugin) : BaseCommand() {
     @Default
     @CommandCompletion("@ownhome")
     @Description("Vous téléporte aléatoirement autour du spawn.")
-    fun onDefault(player: Player, @Default("home") name: String) {
+    fun onDefault(player: Player, home: Home) {
 
         val now = Clock.System.now().plus(teleportConfig.delayBetweenTeleports, DateTimeUnit.SECOND)
             .toLocalDateTime(TimeZone.currentSystemDefault())
